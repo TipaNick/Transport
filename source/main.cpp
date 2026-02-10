da7211fa1033a2c0ade457ca2548893bed494291
Egor Chasovskih <visualjaw3@gmail.com>
2026-02-10 13:00:48 +0700

    added logic

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 
-using namespace std;
+#include "transportFactory.h"
+
+int validateNumber(const char* str) {
+    try {
+        int num = stoi(str);
+        return num;
+    }
+    catch (...) {
+        return -1;
+    }
+}
 
 int main(int argc, char* argv[])
 {
     //for correct russian output 
     setlocale(LC_ALL, "Russian");
 
-    cout << "Первый запуск" << endl;
+    auto bike = TransportFactory::createBike(2, 120, "Electric");
+    auto scooter = TransportFactory::createScooter(2, 15);
+    auto car = TransportFactory::createCar(4, 395, 120);
+    auto bus = TransportFactory::createBus(4, 120, 32);
+
+    for (int i = 1; i < argc; i++) {
+        //check arg is not string
+        int num = validateNumber(argv[i]);
+
+        if (num == 0) { bike->printInfo(); }
+        else if (num == 1) { scooter->printInfo(); }
+        else if (num == 2) { car->printInfo(); }
+        else if (num == 3) { bus->printInfo(); }
+        else { cout << "НЕИЗВЕСТНЫЙ ТИП ТРАНСПОРТА" << endl; }
+
+        cout << endl;
+    }
 }
