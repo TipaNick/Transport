264e66fb2e2d28c744f11d37d0fed87a2d8daa6b
Egor Chasovskih <visualjaw3@gmail.com>
2026-02-10 12:26:11 +0700

    first commit

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //for correct russian output 
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Первый запуск" << endl;
+}
